b2e01ef433e80d9710f38bcc19037629789a5aec
GRINDERT <raihanagargaissa@gmail.com>
2022-07-05 12:14:21 -0700

    nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <holberton.h>
+#include <main.h>
 
 /**
  * it is about writing a text
@@ -6,13 +6,6 @@
  */
 int main(void)
 {
-	char *sh = "Holberton";
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
 
 	return (0);
 }
